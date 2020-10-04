558e9aafad7a96dcbb3aff16f2948731434a22c5
september <alegaon@outlook.com>
2020-10-04 13:05:01 -0300

    Clase 3 Proyecto Integrador v-1

---
+++ laboratorio/3ra_clase/proyecto_integrador.py
@@ -0,0 +1,6 @@
+# presentacion del programa
+
+print("Ingrese el número de la operación que desea ejecutar:")
+print("1 - Ver la lista de alumnos.")
+print("2 - Añadir un alumno a la lista.")
+print("3 - Salir.")
